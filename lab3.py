160ddc9b694dfd49fcd3049e430cfe1bafb2d855
Santiago Luna <sluna2023@fau.edu>
2026-09-15 11:38:09 -0700

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -1,4 +1,8 @@
-name = "sluna2023"
-numbers = {1, 2, 3, 4, 5}
+name = "Santiago Luna"
+numbers = {1000, 2000, 3000, 4000, 5000}
+campus = "Boca Raton, Florida"
+team = "Real Madrid"
 print("Student:", name)
 print("Numbers:", numbers)
+print("Campus:", campus)
+print("Team:", team)
